8037c24a9f5f1e2901a561f0df526f2750e3e38d
MehulSuthar-000 <mehulsuthar858@gmail.com>
2023-04-29 12:44:22 +0530

    commiting two programs into the repository

---
+++ pw.py
@@ -0,0 +1,18 @@
+#!C:\Users\mehul\AppData\Local\Programs\Python\Python310\python.exe
+# pw.py - An insecure password locker program.
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+ 'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+ 'luggage': '12345'}
+
+import sys
+if len(sys.argv) < 2:
+ print('Usage: python pw.py [account] - copy account password')
+ sys.exit()
+account = sys.argv[1] # first command line arg is the account name
+
+import pyperclip
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
